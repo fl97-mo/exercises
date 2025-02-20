d0514df08fa49ede884ae4c5ad705ccea9f37017
FlorianMorina97 <Florian.Morina.1997@outlook.com>
2025-02-20 23:27:13 +0100

    twosum leetcode

--- leet/TwoSum.py
+++ leet/TwoSum.py
@@ -1,10 +1,11 @@
 def twoSum(nums, target):
+    
         seen = {}  
         for i, num in enumerate(nums):
             complement = target - num
             if complement in seen:
                 return [seen[complement], i]
-            seen[num] = i
+            seen[num] = i 
         
         
         
