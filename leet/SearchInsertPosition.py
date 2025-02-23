3a6240849321363662717763ead5324c99586ddc
FlorianMorina97 <florian.morina.1997@outlook.com>
2025-02-23 17:03:35 +0100

    optimized time complexity via binarysearch

--- leet/SearchInsertPosition.py
+++ leet/SearchInsertPosition.py
@@ -1,4 +1,4 @@
-#Given a sorted array of distinct integers and a target value, return the index if the target is found. 
+#Given a sorted array of distinct integers and a target value, return the index if the target is found.
 #If not, return the index where it would be if it were inserted in order.
 #You must write an algorithm with O(log n) runtime complexity.
 #Example 1:
@@ -7,17 +7,21 @@
 #Example 2:
 #Input: nums = [1,3,5,6], target = 2
 #Output: 1
-
+# optimized time complexity
 def searchInsert(nums, target):
+    start = 0
+    end = len(nums) - 1
 
-        for i in range(len(nums)):
-            if nums[i] == target:
-                return i
-            elif len(nums) > 1 and nums[i] <= target <= nums[i+1]:
-                return i + 1
-            elif nums[0] > target:
-                return 0
-            elif nums[-1] < target:
-                return len(nums)
+    while start <= end:
 
-print(searchInsert([1,3,5,6],7))
+            mid = (start + end) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] <= target:
+                start = mid +1
+            elif nums[mid] >= target:
+                end = mid -1
+            
+    return start
+
+print(searchInsert([1,2,3,4,6,7,8,9],5))
